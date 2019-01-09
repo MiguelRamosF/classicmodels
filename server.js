d74312f87d9238cef97314d0ab33e1a0928da984
Miguel <miguel-ramos@hotmail.fr>
2019-01-09 13:04:41 +0100

    aws settings

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 var cors = require('cors')
+const path = require('path');
 
 // set up express app
 const app = express();
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 // connect to mongodb
 mongoose.connect('mongodb://localhost/classicmodels');
